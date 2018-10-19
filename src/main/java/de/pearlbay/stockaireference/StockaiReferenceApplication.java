783ecfce874d16c9fbda8ab2c76236fc194a273e
joern <joernross@gmx.net>
2018-10-19 03:56:10 +0200

    first commit

---
+++ src/main/java/de/pearlbay/stockaireference/StockaiReferenceApplication.java
@@ -0,0 +1,12 @@
+package de.pearlbay.stockaireference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockaiReferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockaiReferenceApplication.class, args);
+	}
+}
