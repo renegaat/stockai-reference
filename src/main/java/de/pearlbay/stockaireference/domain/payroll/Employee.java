6273f435563d95dc0d0262d51f0b6b684f0e2842
joern <joernross@gmx.net>
2018-10-20 15:33:52 +0200

    added employee pojo

---
+++ src/main/java/de/pearlbay/stockaireference/domain/payroll/Employee.java
@@ -0,0 +1,21 @@
+package de.pearlbay.stockaireference.domain.payroll;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Employee {
+    
+    private @Id @GeneratedValue Long id;
+    private String name;
+    private String role;
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
