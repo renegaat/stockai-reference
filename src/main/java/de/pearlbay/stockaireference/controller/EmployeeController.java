40595388a772d49935daa3c0fbafa9a9ee520181
joern <joernross@gmx.net>
2018-10-23 08:05:03 +0200

    modified rest result.

--- src/main/java/de/pearlbay/stockaireference/controller/EmployeeController.java
+++ src/main/java/de/pearlbay/stockaireference/controller/EmployeeController.java
@@ -3,11 +3,17 @@ package de.pearlbay.stockaireference.controller;
 import de.pearlbay.stockaireference.application.EmployeeNotFoundException;
 import de.pearlbay.stockaireference.domain.payroll.Employee;
 import de.pearlbay.stockaireference.repository.EmployeeRepository;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -19,15 +25,25 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    Resources<Resource<Employee>> all() {
+        List<Resource<Employee>> employees = repository.findAll().stream()
+            .map(employee -> new Resource<>(employee,
+                    linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                    linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return new Resources<>(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
-
-
+    
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id){
-        
-        return repository.findById(id)
+    Resource<Employee> one(@PathVariable Long id) {
+
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        return new Resource<>(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 }
